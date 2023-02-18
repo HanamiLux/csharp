f55a0fb1684c908b1474c5f99fcb70fe0ec7fa67
HanamiLux <oxotnikzn2017@yandex.ru>
2023-02-18 15:17:54 +0300

    megaok

---
+++ Diary/Note.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Diary
+{
+    class Note
+    {
+        public string name, description;
+        public DateTime currentDate, date;
+        public Note(string name, string description, DateTime date)
+        {
+            this.name = name;
+            this.description = description;
+            currentDate = DateTime.Now;
+            this.date = date;
+        }
+    }
+}
