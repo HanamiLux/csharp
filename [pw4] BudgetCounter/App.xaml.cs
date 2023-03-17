ceae022593b9cbc851d73a4ec2294cc98af1bd6f
HanamiLux <oxotnikzn2017@yandex.ru>
2023-03-18 02:27:47 +0300

    Speedrunned budget

---
+++ [pw4] BudgetCounter/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Diary
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
